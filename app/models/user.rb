2f2d97f8424bc570476bb21a130dd99641b57cdd
nilooravaei <nilooravaei@gmail.com>
2016-01-28 19:42:54 +0000

    updated user model to encrypt password

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,4 @@ class User < ActiveRecord::Base
 
   validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/, message: "must follow email format" }
 
-  def authenticate(p)
-    password == p
-  end 
-
 end 
