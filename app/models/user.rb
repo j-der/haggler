555e25d425f7a0a2558c3993128cfddaac377ecf
nilooravaei <nilooravaei@gmail.com>
2016-01-28 19:24:55 +0000

    bundle version conflict

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+  has_secure_password
+
   has_many :posts, dependent: :destroy
 
   validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/, message: "must follow email format" }
